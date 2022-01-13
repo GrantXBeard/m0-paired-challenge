27df6e5f8f1d31902b3a75d170fb5af3d75622d5
Grant Beard <Grantxbeard@gmail.com>
2022-01-12 21:42:07 -0700

    initial commit

---
+++ challenge/books.js
@@ -0,0 +1,5 @@
+var names = "Titles"
+var amount = 5
+var hasRead = FALSE
+var checkedOut = TRUE
+var late =
---
+++ challenge/index.md
@@ -0,0 +1,5 @@
+# Book Titles
+- Harry potter
+- Lord of the readings
+- Catcher in the rye
+- 1984
